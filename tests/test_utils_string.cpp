a0c7fdfd65ac31af431a038cf6e770a4b0ca9b47
Matej Zavrsnik <matejzavrsnik@gmail.com>
2016-07-26 00:17:28 +0100

    it would be nice if I could spell algorithm names

--- tests/test_utils_string.cpp
+++ tests/test_utils_string.cpp
@@ -77,134 +77,134 @@ TEST_F(fixture_utils_string, split_string_puctuation_all_punctuations)
    ASSERT_EQ(result[10], "\n");
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_basic) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_basic) 
 {
    std::string str1 = "matej";
    std::string str2 = "mtey";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 omitted a, 1 substituted y : edit distance = 2
    ASSERT_EQ(2, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_equal) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_equal) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // it's the same
    ASSERT_EQ(0, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_a_complicated_one) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_a_complicated_one) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "mate zavrsnick lykes startrek but and very much the original series because it is too naive.";
    //                     d        a   s        d        sss ssaa   -> 11 changes
    // d: deletion, a: addition, s: substitution
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // a bunch of edits
    ASSERT_EQ(11, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_substition_first) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_substition_first) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "natej zavrsnik likes star trek but not so much the original series because it is too naive.";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 substitution
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_substition_last) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_substition_last) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "matej zavrsnik likes star trek but not so much the original series because it is too naive!";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 substitution
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_substition_middle) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_substition_middle) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "matej zavrsnik likes star trek but not so much theAoriginal series because it is too naive.";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 substitution
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_deletion_first) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_deletion_first) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 =  "atej zavrsnik likes star trek but not so much the original series because it is too naive.";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 deletion
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_deletion_last) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_deletion_last) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "matej zavrsnik likes star trek but not so much the original series because it is too naive";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 deletion
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_deletion_middle) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_deletion_middle) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "matej zavrsnik likes star trek but not so much the original eries because it is too naive.";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 deletion
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_insertion_first) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_insertion_first) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "Qmatej zavrsnik likes star trek but not so much the original series because it is too naive.";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 insertion
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_insertion_last) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_insertion_last) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.Q";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 insertion
    ASSERT_EQ(1, result);
 }
 
-TEST_F(fixture_utils_string, wagner_fisher_distance_insertion_middle) 
+TEST_F(fixture_utils_string, wagner_fischer_distance_insertion_middle) 
 {
    std::string str1 = "matej zavrsnik likes star trek but not so much the original series because it is too naive.";
    std::string str2 = "matej zavrsnik likes star trek but not so much the original Qseries because it is too naive.";
    
-   int result = mzlib::util::wagner_fisher_distance(str1, str2);
+   int result = mzlib::util::wagner_fischer_distance(str1, str2);
    
    // 1 insertion
    ASSERT_EQ(1, result);
