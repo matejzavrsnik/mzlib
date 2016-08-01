741d1c77cc3290a19e5a46cd62ee60ee0a95741d
Matej Zavrsnik <matejzavrsnik@gmail.com>
2016-08-01 23:34:41 +0100

    added mendel function to steer genetic algo; mendel, get it?

--- include/genetic.h
+++ include/genetic.h
@@ -79,6 +79,8 @@ public:
       return m_genome_pool.size();
    }
    
+   virtual size_t get_problem_space () = 0;
+   
 protected:
 
    cgenetic (const TYPE& seed, ifitness_function fitness_function)
@@ -163,10 +165,16 @@ public:
    };
    
    ~cgenetic_object () {};
+   
+   virtual size_t get_problem_space () override 
+   {
+      return sizeof(TYPE);
+   }
     
 private:
    
-   virtual tgenome_info get_zap_info (tgenome& genome) override {
+   virtual tgenome_info get_zap_info (tgenome& genome) override 
+   {
       tgenome_info info;
       info.genome_size = sizeof(TYPE);
       info.genome_pointer = reinterpret_cast<char*>(&genome.genome);;
@@ -196,9 +204,15 @@ public:
    
    virtual ~cgenetic_container () {};
    
+   virtual size_t get_problem_space () override 
+   {
+      return m_object_size * m_object_count;
+   }
+   
 private:
 
-   virtual tgenome_info get_zap_info (tgenome& genome) override {
+   virtual tgenome_info get_zap_info (tgenome& genome) override 
+   {
       tgenome_info info;
       size_t zap_object = mzlib::util::get_random_integer_between (0, m_object_count);
       info.genome_size = m_object_size;
@@ -211,6 +225,66 @@ private:
    
 };
    
+template <class TYPE>
+TYPE mendel_until_perfect(cgenetic<TYPE>& genetic, double perfect_penalty)
+{
+   while (genetic.get_lowest_penalty() != perfect_penalty) {
+      genetic.play_generations(1);
+   }
+   return genetic.get_best_genome();
+}
+
+template <class TYPE>
+TYPE mendel_smart(cgenetic<TYPE>& genetic)
+{
+   TYPE best_genome_overall = genetic.get_best_genome();
+   double lowest_penalty_overall = genetic.get_lowest_penalty();
+   
+   // settings
+   const double penalty_improvement_threshold = 0.0001;
+   const int allowed_generations_without_improvement = 1000;
+   const int generations_in_one_loop = 100;
+   const int generation_size = 100;
+   const int survival_count = 20;
+   const int max_mutation_rate = genetic.get_problem_space();
+   const double lowest_possible_penalty = 0;
+   
+   // setup
+   double current_penalty, previous_penalty, penalty_improvement;
+   current_penalty = previous_penalty = genetic.get_lowest_penalty();
+   int mutation_rate = 1;
+   genetic.set_mutation_rate(mutation_rate);
+   genetic.set_generation_size(generation_size);
+   genetic.set_survivers_count(survival_count);
+           
+   // make eons fly-by, doubling the mutation rate as generations get "stale"
+   while (mutation_rate < max_mutation_rate && lowest_penalty_overall > lowest_possible_penalty) {
+      int passed_generations_without_improvement = 0;
+      do {
+         genetic.play_generations(generations_in_one_loop);
+         current_penalty = genetic.get_lowest_penalty();
+         penalty_improvement = (previous_penalty - current_penalty) / previous_penalty;
+         if (penalty_improvement < penalty_improvement_threshold) {
+            passed_generations_without_improvement += generations_in_one_loop;
+         }
+         else {
+            mutation_rate = 1;
+         }
+         previous_penalty = current_penalty; 
+      }
+      while (passed_generations_without_improvement < allowed_generations_without_improvement);
+      if (genetic.get_lowest_penalty() < lowest_penalty_overall) {
+         best_genome_overall = genetic.get_best_genome();
+         lowest_penalty_overall = genetic.get_lowest_penalty();
+      }
+      if (mutation_rate < max_mutation_rate) mutation_rate *= 2;
+      if (mutation_rate > max_mutation_rate) mutation_rate = max_mutation_rate;
+      genetic.set_mutation_rate(mutation_rate);
+   }
+   
+   return best_genome_overall;
+}
+
 } // namespace mzlib
 
 #endif	/* GENETIC_H */
--- tests/test_genetic.cpp
+++ tests/test_genetic.cpp
@@ -97,3 +97,36 @@ TEST_F(fixture_genetic, works_on_vector)
    //std::vector<int> numbers_after = genetic.get_best_genome();
    //ASSERT_TRUE(true);
 }
+
+// Delete DISABLED_ to see the function in action
+TEST_F(fixture_genetic, DISABLED_mendel_smart_demo) 
+{
+   std::vector<int> numbers(10);
+   
+   // setup a fitness function
+   auto fitness_function = 
+      [](const std::vector<int>& candidate) {
+         // test criteria: each next number is 2x the previous
+         double sum_difference = 0;
+         for (int i = 0; i<candidate.size()-1; ++i) {
+            // discourage trivial and incorrect solutions
+            // fun fact: in int world, 2 * -2147483648 == 0 !!
+            if (candidate[i] <= 0 || candidate[i] == -2147483648) {
+               return std::numeric_limits<double>::max();
+            }
+            for (int j = i+1; j<candidate.size(); ++j) {
+               int factor = std::pow(2, j-i);
+               sum_difference += std::fabs(candidate[j] - (factor * candidate[i]));
+            }
+         }
+         return sum_difference;
+      };
+      
+   mzlib::cgenetic_container<std::vector<int>> genetic (numbers, fitness_function);
+   
+   double penalty_before = fitness_function(numbers);
+   std::vector<int> numbers_after = mzlib::mendel_smart(genetic);
+   double penalty_after = fitness_function(numbers_after);
+   
+   ASSERT_TRUE(true);
+}
