410352c1ee293e7853874c401b746c6bab8d47d1
Matej Zavrsnik <matejzavrsnik@gmail.com>
2016-10-31 22:11:29 +0000

    Added unhappy path tests to law of acceleration

--- tests/test_laws.cpp
+++ tests/test_laws.cpp
@@ -41,6 +41,15 @@ TEST_F(fixture_laws, acceleration2d_solve_for_force)
    ASSERT_TRUE(direction == unit_vector2d);
 }
 
+TEST_F(fixture_laws, acceleration2d_solve_for_force_missing_argument) 
+{
+   mzlib::law::cacceleration2d acc;
+   //acc.m = 14.17_kg;
+   acc.a = unit_vector2d * 14.14_m_per_s2;
+
+   ASSERT_THROW(acc.solve_for_force(), mzlib::exception::not_set);
+}
+
 TEST_F(fixture_laws, acceleration2d_solve_for_acceleration) 
 {
    mzlib::law::cacceleration2d acc;
@@ -55,6 +64,15 @@ TEST_F(fixture_laws, acceleration2d_solve_for_acceleration)
    ASSERT_TRUE(direction == unit_vector2d);
 }
 
+TEST_F(fixture_laws, acceleration2d_solve_for_acceleration_missing_argument) 
+{
+   mzlib::law::cacceleration2d acc;
+   acc.m = 17.23_kg;
+   //acc.f = unit_vector2d * 10.52_N;
+   
+   ASSERT_THROW(acc.solve_for_acceleration(), mzlib::exception::not_set);
+}
+
 TEST_F(fixture_laws, acceleration2d_solve_for_mass) 
 {
    mzlib::law::cacceleration2d acc;
@@ -65,4 +83,23 @@ TEST_F(fixture_laws, acceleration2d_solve_for_mass)
    auto size = acc.m.get();
    
    ASSERT_TRUE(mzlib::dbl(size).equals(0.90839694656488568L));
+}
+
+TEST_F(fixture_laws, acceleration2d_solve_for_mass_missing_argument) 
+{
+   mzlib::law::cacceleration2d acc;
+   //acc.f = unit_vector2d * 16.66_N;
+   acc.a = unit_vector2d * 18.34_m_per_s2;
+   
+   ASSERT_THROW(acc.solve_for_mass(), mzlib::exception::not_set);
+}
+
+TEST_F(fixture_laws, acceleration2d_solve_for_mass_preconditions_not_met) 
+{
+   mzlib::law::cacceleration2d acc;
+   acc.f = -unit_vector2d * 16.66_N;
+   acc.a = unit_vector2d * 18.34_m_per_s2;
+   
+   // force and accelerations are not colinear; this law can't be used
+   ASSERT_THROW(acc.solve_for_mass(), mzlib::exception::invalid_values);
 }
