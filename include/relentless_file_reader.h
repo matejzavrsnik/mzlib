a97ae17bff829336dd8bdb21bf8a69fe2f02617e
Matej Zavrsnik <matejzavrsnik@gmail.com>
2016-12-04 19:04:15 +0000

    Added aggregated file monitor

--- include/relentless_file_reader.h
+++ include/relentless_file_reader.h
@@ -16,23 +16,38 @@
 
 namespace mzlib {
 
-// Keep reading file and if something is added, report via callback
+// Reports the changes to a file via callback
 class relentless_file_reader
 {
+
+public:
+   
+   // First argument is new content, second is the filename
+   using type_callback = std::function<void(const std::string&, const std::string&)>;
    
 private:
    
    const std::string m_filename;
    std::streampos m_last_read_position;
+   type_callback m_callback;
    
 public:
-   using type_callback = std::function<void(const std::string&, const std::string&)>;
-   type_callback callback;
 
-   relentless_file_reader(const std::string& filename) :
+   relentless_file_reader(const std::string& filename, type_callback callback) :
       m_filename(filename),
-      m_last_read_position(find_eof_position(filename))
+      m_last_read_position(find_eof_position(filename)),
+      m_callback(callback)
    {
+   }
+   
+   relentless_file_reader(const std::string& filename) :
+      relentless_file_reader(filename, nullptr)
+   {
+   }
+   
+   void set_callback(type_callback callback)
+   {
+      m_callback = callback;
    }
    
    void anything_new_what_about_now() 
@@ -41,9 +56,45 @@ public:
       if (file_eof_position > m_last_read_position) {
          const std::string new_content = read_file(m_filename, m_last_read_position, file_eof_position);
          m_last_read_position = file_eof_position;
-         callback(new_content, m_filename);
+         m_callback(new_content, m_filename);
       }
    }  
+   
+};
+
+// Aggregates relentless file readers to work on a collection of files
+class aggregated_file_monitor
+{
+
+private:
+   
+   std::vector<relentless_file_reader> m_readers;
+   
+public:
+   
+   aggregated_file_monitor(
+      const std::vector<std::string>& files,
+      relentless_file_reader::type_callback callback)
+   {
+      for (const std::string& file : files) {
+         m_readers.emplace_back(file, callback);
+      }
+   }
+   
+   void inspect_all()
+   {
+      for (auto& reader : m_readers) {
+         reader.anything_new_what_about_now();
+      }
+   }
+   
+   void set_callback(relentless_file_reader::type_callback callback)
+   {
+      for (auto& reader : m_readers) {
+         reader.set_callback(callback);
+      }
+   }
+           
 };
 
 } // namespaces
--- tests/test_relentless_file_reader.cpp
+++ tests/test_relentless_file_reader.cpp
@@ -23,44 +23,104 @@ protected:
    
    virtual void SetUp() 
    {
-      m_test_file = m_test_dir + "/temp_file";
+      m_test_file1 = m_test_dir + "/temp_file1";
       std::string sample_content = "I must not fear.";
-      mzlib::save_file (m_test_file, sample_content);
+      mzlib::save_file (m_test_file1, sample_content);
+      
+      m_test_file2 = m_test_dir + "/temp_file2";
+      sample_content = "Fear is the little-death that brings total obliteration.";
+      mzlib::save_file (m_test_file2, sample_content);
    }
    virtual void TearDown() 
    {
-      std::remove (m_test_file.c_str());
+      std::remove (m_test_file1.c_str());
+      std::remove (m_test_file2.c_str());
    }
    
    std::string m_test_dir;
-   std::string m_test_file;
+   std::string m_test_file1;
+   std::string m_test_file2;
 };
 
 TEST_F(fixture_relentless_file_reader, basic_test)
 {
    std::string file;
    std::string content;
+   bool was_called;
    
-   mzlib::relentless_file_reader reader(m_test_file);
+   mzlib::relentless_file_reader reader(m_test_file1);
    
-   reader.callback = 
-      [] (const std::string&, const std::string&) {
-         FAIL(); // should not be called next
-      };
+   was_called = false;
+   reader.set_callback( 
+      [&was_called] (const std::string&, const std::string&) {
+         was_called = true;
+      });
    reader.anything_new_what_about_now();
+   ASSERT_FALSE(was_called); // should not be called
    
    std::string additional_content = "Fear is the mind-killer.";
-   mzlib::append_file (m_test_file, additional_content);
-   reader.callback = 
-      [this] (const std::string& addition, const std::string& file) {
-         ASSERT_EQ("Fear is the mind-killer.", addition);
-         ASSERT_EQ(m_test_file, file);
-      };
+   mzlib::append_file (m_test_file1, additional_content);
+   reader.set_callback( 
+      [&content, &file] (const std::string& c, const std::string& f) {
+         content = c;
+         file = f;
+      });
    reader.anything_new_what_about_now();
+   ASSERT_EQ("Fear is the mind-killer.", content);
+   ASSERT_EQ(m_test_file1, file);
+
+   was_called = false;   
+   reader.set_callback( 
+      [&was_called] (const std::string&, const std::string&) {
+         was_called = true;
+      });
+   reader.anything_new_what_about_now();
+   ASSERT_FALSE(was_called); // should not be called
+}
+
+TEST_F(fixture_relentless_file_reader, aggregated_file_monitor_basic_test)
+{
+   std::vector<std::string> files;
+   files.push_back(m_test_file1);
+   files.push_back(m_test_file2);
    
-   reader.callback = 
-      [] (const std::string&, const std::string&) {
-         FAIL(); // should not be called next
-      };
-   reader.anything_new_what_about_now();
+   struct treturns {
+      std::string file;
+      std::string content;
+   };
+   std::vector<treturns> returns;
+   bool was_called;
+   
+   mzlib::aggregated_file_monitor monitor(files, nullptr);
+   
+   was_called = false;
+   monitor.set_callback( 
+      [&was_called] (const std::string&, const std::string&) {
+         was_called = true;
+      });
+   monitor.inspect_all();
+   ASSERT_FALSE(was_called); // should not be called
+   
+   std::string additional_content = "Fear is the mind-killer.";
+   mzlib::append_file (m_test_file1, additional_content);
+   additional_content = "I will face my fear.";
+   mzlib::append_file (m_test_file2, additional_content);
+   monitor.set_callback( 
+      [&returns] (const std::string& c, const std::string& f) {
+         returns.push_back(treturns{f,c});
+      });
+   monitor.inspect_all();
+   ASSERT_EQ(2, returns.size());
+   ASSERT_EQ(m_test_file1, returns[0].file);
+   ASSERT_EQ("Fear is the mind-killer.", returns[0].content);
+   ASSERT_EQ(m_test_file2, returns[1].file);
+   ASSERT_EQ("I will face my fear.", returns[1].content);
+
+   was_called = false;   
+   monitor.set_callback( 
+      [&was_called] (const std::string&, const std::string&) {
+         was_called = true;
+      });
+   monitor.inspect_all();
+   ASSERT_FALSE(was_called); // should not be called
 }
