6878938cc91c0c73416580f4a85ebd2afa04daa9
Matej Zavrsnik <matejzavrsnik@gmail.com>
2017-03-04 23:37:51 +0000

    added capability to return description with mzlib exceptions

--- include/exceptions.h
+++ include/exceptions.h
@@ -9,20 +9,46 @@
 #define	MZLIB_EXCEPTIONS_H
 
 #include <exception>
+#include <string>
 
 namespace mzlib {
+
 // purpose of this nested namespace: it better explains the purpose of these class
 // names when used in code. is_empty on itself means nothing, exception::is_empty
 // does. To make it more readable using other means I would need to prepend
 // "exception" to every class name, but that is better done using nested namespace.
 namespace exception {
+
+class generic : public std::exception
+{
+
+private:
+
+   std::string m_explanation;
+
+public:
    
-class is_empty        : public std::exception {};
-class not_set         : public std::exception {};
-class not_found       : public std::exception {};
-class invalid_values  : public std::exception {};
-class not_implemented : public std::exception {}; // java influence on me, I guess
-class meaningless     : public std::exception {};
+   generic() 
+   {
+   }
+   
+   generic(std::string explanation)
+      : m_explanation(explanation) 
+   {
+   }
+   
+   const char* what() const noexcept override
+   { 
+      return m_explanation.c_str();
+   }
+};
+   
+class is_empty        : public generic { public: using generic::generic; };
+class not_set         : public generic { public: using generic::generic; };
+class not_found       : public generic { public: using generic::generic; };
+class invalid_values  : public generic { public: using generic::generic; };
+class not_implemented : public generic { public: using generic::generic; };
+class meaningless     : public generic { public: using generic::generic; };
 
 } } // namespace
 
