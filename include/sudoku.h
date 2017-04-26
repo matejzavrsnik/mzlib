eee9b7ef983bff96d08b3ae54d4a4e03e4da9ff4
Matej Zavrsnik <matejzavrsnik@gmail.com>
2017-04-26 19:11:13 +0100

    added backtracking solver for killer sudoku; it is slooooooo...

--- include/sudoku.h
+++ include/sudoku.h
@@ -13,10 +13,12 @@
 #include <vector>
 #include <iostream>
 #include <initializer_list>
+#include <algorithm>
 
 namespace mzlib {
    
-class sudoku {
+class sudoku 
+{
    
 public:
    
@@ -24,7 +26,7 @@ public:
    using cell_coordinates = mzlib::vector<uint, 2>;
    const short has_no_value = 0;
     
-   sudoku(const std::initializer_list<short> puzzle)
+   sudoku(const std::initializer_list<short>& puzzle)
    {
       set_puzzle(puzzle);
    }
@@ -35,6 +37,11 @@ public:
    ~sudoku() = default;
     
    void set_puzzle(const std::initializer_list<short>& puzzle)
+   {
+      m_puzzle.assign(puzzle.begin(), puzzle.end());
+   }
+   
+   void set_puzzle(const puzzle& puzzle)
    {
       m_puzzle.assign(puzzle.begin(), puzzle.end());
    }
@@ -53,7 +60,7 @@ public:
       }
       solve(start);
    }
-    
+   
 private:
 
    puzzle m_puzzle;
@@ -73,6 +80,7 @@ private:
             *curr = number;
             if(is_value_acceptable(curr))
             {
+               //simple_out();
                auto next = get_next_cell_without_value(curr);
                solve(next);
             }
@@ -82,6 +90,24 @@ private:
            *curr = 0;
         }
       }
+   }
+   
+   void simple_out ()
+   {
+      for(uint i=0; i<80; ++i) {
+         if(m_puzzle[i] == 0) break;
+         std::cout << m_puzzle[i];
+         if((i+1)%3==0) std::cout << " ";
+         if((i+1)%9==0) std::cout << " ";
+      }
+      std::cout << std::endl;
+   }
+   
+   cell_coordinates get_coordinates_of_cell(puzzle::const_iterator it) const
+   {
+      int index = std::distance(m_puzzle.cbegin(), it);
+      cell_coordinates coor = mzlib::get_coordinates_from_index(index, 9);
+      return coor;
    }
    
    bool is_value_acceptable(puzzle::iterator current) const
@@ -95,16 +121,19 @@ private:
       if (is_number_repeated_inside_square(coor)) {
          return false;
       }
+      
+      if (value_fails_additional_restrictions(coor)) {
+         return false;
+      }
 
       // if all checks passed, puzzle must be valid for this cell
       return true;
    }
    
-   cell_coordinates get_coordinates_of_cell(puzzle::const_iterator it) const
+   virtual bool value_fails_additional_restrictions(cell_coordinates) const
    {
-      int index = std::distance(m_puzzle.cbegin(), it);
-      cell_coordinates coor = mzlib::get_coordinates_from_index(index, 9);
-      return coor;
+      // an opportunity for extensions to implement additional restrictions
+      return false;
    }
    
    bool is_number_repeated_in_row_or_column(cell_coordinates coordinates) const
@@ -151,6 +180,105 @@ private:
       return std::find(curr, m_puzzle.end(), has_no_value);
    }
 
+};
+
+
+class killer_sudoku : public sudoku 
+{
+
+public:
+   
+   struct region_sum
+   {
+      short region;
+      int sum;
+   };
+   
+   using regions = std::vector<short>;
+   using region_sums = std::vector<region_sum>;
+   
+   killer_sudoku(
+      const puzzle& puzzle,
+      const regions& regions,
+      const region_sums& region_sums)
+   {
+      set_puzzle(puzzle);
+      set_regions(regions);
+      set_region_sums(region_sums);
+   }
+
+   killer_sudoku() = default;
+   killer_sudoku(const killer_sudoku& orig) = default;
+   killer_sudoku& operator=(const killer_sudoku& orig) = default;
+   ~killer_sudoku() = default;
+   
+   void set_regions(const regions& regions)
+   {
+      m_regions.assign(regions.begin(), regions.end());
+   }
+   
+   void set_region_sums(const region_sums& region_sums)
+   {
+      m_region_sums.assign(region_sums.begin(), region_sums.end());
+   }
+      
+private:
+   
+   bool value_fails_additional_restrictions(cell_coordinates coordinates) const override
+   {
+      if (number_exceeds_sum_in_region(coordinates)) {
+         return true;
+      }
+      return false;
+   }
+   
+   bool number_exceeds_sum_in_region(cell_coordinates coordinates) const
+   {
+      short region = get_region(coordinates);
+      int sum_in_region = get_sum_of_region(region);
+
+      int partial_sum = 0;
+      auto region_it = m_regions.begin(); 
+      while((region_it = std::find(region_it, m_regions.end(), region)) != m_regions.end())
+      {
+         auto coor = get_coordinates_of_region(region_it);
+         partial_sum += get_cell(coor);
+         if(partial_sum > sum_in_region) {
+            return true;
+         }
+         ++region_it;
+      };
+      
+      return false;
+   }
+   
+   short get_region(cell_coordinates coordinates) const
+   {
+      auto index = mzlib::get_index_from_coordinates(coordinates, 9);
+      return m_regions[index];
+   }
+   
+   int get_sum_of_region(short region) const
+   {
+      auto region_sum_it = std::find_if(m_region_sums.begin(), m_region_sums.end(), 
+         [&region](const region_sum& s) {
+            return s.region == region;
+         });
+      if (region_sum_it == m_region_sums.end()) {
+         return 0;
+      }
+      return region_sum_it->sum;
+   }
+   
+   cell_coordinates get_coordinates_of_region(regions::const_iterator it) const
+   {
+      int index = std::distance(m_regions.cbegin(), it);
+      cell_coordinates coor = mzlib::get_coordinates_from_index(index, 9);
+      return coor;
+   }
+   
+   regions m_regions;
+   region_sums m_region_sums;
 };
 
 } // namespaces
