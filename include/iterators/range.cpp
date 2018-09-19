62a130d44dc9c34f1bfa0e72291df361fa5cc9b7
Matej Zavrsnik <matejzavrsnik@gmail.com>
2018-09-19 20:31:51 +0100

    added ranges

---
+++ include/iterators/range.cpp
@@ -0,0 +1,60 @@
+//
+// Copyright (c) 2018 Matej Zavrsnik
+//
+// Web:  matejzavrsnik.com
+// Mail: matejzavrsnik@gmail.com
+//
+
+#include "gtest/gtest.h"
+
+#include "range.h"
+#include <vector>
+
+TEST(range, distance)
+{
+   std::vector<int> v = {1,2,3,4,5,6,7};
+   using it = std::vector<int>::iterator;
+   
+   auto range_begin = v.begin()+1; // at 2
+   auto range_end = v.begin()+3; // at 4
+   
+   mzlib::range<it> r(range_begin, range_end);
+   
+   auto std_distance = std::distance(range_begin, range_end);
+   auto range_distance = r.distance();
+   
+   ASSERT_EQ(std_distance, range_distance);
+}
+
+TEST(range, advance)
+{
+   std::vector<int> v = {1,2,3,4,5,6,7};
+   using it = std::vector<int>::iterator;
+   
+   auto range_begin = v.begin()+1; // at 2
+   auto range_end = v.begin()+3; // at 4
+   
+   mzlib::range<it> r(range_begin, range_end);
+   r.advance(1);
+   
+   ASSERT_EQ(++range_begin, r.beg());
+   ASSERT_EQ(++range_end, r.end());
+}
+
+TEST(range, advance_preserves_distance)
+{
+   std::vector<int> v = {1,2,3,4,5,6,7};
+   using it = std::vector<int>::iterator;
+   
+   auto range_begin = v.begin()+1; // at 2
+   auto range_end = v.begin()+3; // at 4
+   
+   mzlib::range<it> r(range_begin, range_end);
+   auto distance_before = r.distance();
+   
+   r.advance(1);
+   
+   auto distance_after = r.distance();
+   
+   ASSERT_EQ(distance_before, distance_after);
+}
---
+++ include/iterators/range.h
@@ -0,0 +1,63 @@
+//
+// Copyright (c) 2018 Matej Zavrsnik
+//
+// Web:  matejzavrsnik.com
+// Mail: matejzavrsnik@gmail.com
+//
+
+#ifndef MZLIB_RANGE_H
+#define MZLIB_RANGE_H
+
+namespace mzlib {
+
+
+// for when you want to:
+// - the function to return begin and end at the same time
+// - advance begin and end for the same amount
+// - save calls to std::distance with the same begin and end
+
+template<typename It>
+class range 
+{
+
+private:
+      
+   mutable int m_distance = -1;
+   It m_begin, m_end;
+   
+public:
+   
+   range(It begin, It end) :
+      m_distance(-1),
+      m_begin(begin),
+      m_end(end)
+   {}
+      
+   const It& beg() 
+   {
+      return m_begin;
+   }
+   
+   const It& end() 
+   { 
+      return m_end; 
+   }
+   
+   void advance(size_t distance)
+   {
+      std::advance(m_begin, distance);
+      std::advance(m_end, distance);
+   }
+   
+   size_t distance()  const
+   {
+      // lazy evaluate and remember
+      if(m_distance<0)
+         m_distance = std::distance(m_begin, m_end);
+      return m_distance;
+   }
+};
+
+} // namespace
+
+#endif // MZLIB_RANGE_H
