9f07f9de3fa9d53d4de29b363bad538be36fb4fd
Matej Zavrsnik <matejzavrsnik@gmail.com>
2019-06-25 22:10:42 +0100

    converter from std::pair to point2d

--- include/tools/converters.h
+++ include/tools/converters.h
@@ -38,7 +38,25 @@ inline Glib::ustring convert(
 
 #endif // _GLIBMM_USTRING_H
 
+// 2-dimensional mzlib::vector <-> std::pair
+#ifdef MZLIB_VECTOR_H
 
+// todo: figure out how to do this specialization for all types, not just double
+template<>
+inline vector<double, 2> convert(
+   const std::pair<double,double>& pair)
+{
+   return {pair.first, pair.second};
+}
+
+template<>
+inline std::pair<double,double> convert(
+   const vector<double, 2>& vec)
+{
+   return {vec[0], vec[1]};
+}
+
+#endif // MZLIB_VECTOR_H
 
 } // namespace mzlib
 
