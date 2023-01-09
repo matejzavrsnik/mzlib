6dc58978fecccb293268d346900b6af914170dc5
matej <matejzavrsnik@gmail.com>
2023-01-09 19:39:10 +0000

    puting this aside for a while...

--- include/tools/nested_vector.h
+++ include/tools/nested_vector.h
@@ -34,6 +34,15 @@ void enlarge(
          v.resize(col_count);
 }
 
+template<typename T>
+void add_top(
+   type<T>& vv,
+   int row_count,
+   const T& val)
+{
+   vv.insert(vv.begin(), row_count, std::vector<T>(vv[0].size(), val));
+}
+
 // Value is not universal reference, because unless this is
 // a special case with size 1x1, it needs to be in multiple
 // locations anyway, and there is no way to put it there by
@@ -60,6 +69,69 @@ T& access(
    // works with count. That's why they are incremented 
    enlarge(vv, row_index+1, col_index+1);  
    return vv[row_index][col_index];
+}
+
+// Read only access
+template<typename T>
+const T& access(
+   const type<T>& vv,
+   std::size_t row_index,
+   std::size_t col_index)
+{
+   // Read-only version cannot enlarge
+   if (vv.size() == 0 || row_index >= vv.size() || col_index >= vv[0].size())
+      throw mzlib::exception::out_of_range();
+   return vv[row_index][col_index];
+}
+
+// Overload for using coordinates (x,y)-system to access field (row, col)-system
+template<typename T>
+T& access(
+   type<T>& vv,
+   const mzlib::coordinates2d& coordinates)
+{
+   // Tricky: they are swapped. In normal coordinate system, second dimension, y, is up-down, whereas
+   // in nested vectors, up-down is the first dimension.
+   return access(vv, coordinates[1], coordinates[0]);
+}
+
+// When you need this overload for read-only
+template<typename T>
+const T& access(
+   const type<T>& vv,
+   const mzlib::coordinates2d& coordinates)
+{
+   // Tricky: they are swapped. In normal coordinate system, second dimension, y, is up-down, whereas
+   // in nested vectors, up-down is the first dimension.
+   return access(vv, coordinates[1], coordinates[0]);
+}
+
+// It's good to have these things named and implemented in one
+// place rather than keep thinking which dimension represents
+// width and height
+template<typename T>
+auto width(
+   const type<T>& vv) -> decltype(vv.size())
+{
+   // vv[rows = height][cols = width]
+   return vv.size()==0 ? 0 : vv[0].size();
+}
+
+template<typename T>
+auto height(
+   const type<T>& vv) -> decltype(vv.size())
+{
+   // vv[rows = height][cols = width]
+   return vv.size();
+}
+
+// useful because you can add the coordinates vector to other vectors
+template<typename T>
+mzlib::coordinates2d size(
+   const type<T>& vv)
+{
+   // todo: avoid narrowing
+   return mzlib::coordinates2d{(int)width(vv), (int)height(vv)};
 }
 
 } // namespace nested_vector
