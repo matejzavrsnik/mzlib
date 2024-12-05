f2ff0a7262c5cf5844716fd0e4ee60d0aad1d7ae
matej <matejzavrsnik@gmail.com>
2024-12-05 22:09:12 +0000

    added all grid include header

---
+++ include/grid/all.h
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2024 Matej Zavrsnik
+//
+// Web:  matejzavrsnik.com
+// Mail: matejzavrsnik@gmail.com
+//
+
+#pragma once
+
+#include "apply_drawing.h"
+#include "are_touching.h"
+#include "check_for_drawing.h"
+#include "convert_to_grid.h"
+#include "drawing_operation.h"
+#include "exists_in.h"
+#include "fits_on_grid.h"
+#include "get_all_cells.h"
+#include "get_all_cells_with_value.h"
+#include "get_direction_cartesian.h"
+#include "get_neighbour_cells.h"
+#include "grid.h"
+#include "is_in.h"
+#include "manhattan_distance.h"
+#include "move_cartesian.h"
+#include "move_drawing.h"
+#include "move_screen.h"
+#include "move_to_next_cell.h"
+#include "move_unless.h"
+#include "set_all_to.h"
+#include "shortest_path_bfs.h"
--- include/grid/check_for_drawing.h
+++ include/grid/check_for_drawing.h
@@ -19,32 +19,6 @@ namespace grid
 
 template <typename T>
 bool
-apply_drawing (
-   mzlib::grid::type<T>& grid,
-   const mzlib::grid::type<T>& drawing,
-   const mzlib::grid::cell& c,
-   const drawing_operation operation = drawing_operation::add
-)
-{
-   // todo: if drawing too big, draw what fits and ignore the rest. currently I crash instead.
-   mzlib::grid::cell drawing_pixel{0, 0};
-   const mzlib::grid::cell drawing_size = mzlib::grid::size(drawing);
-   do
-   {
-      auto canvas_pixel = drawing_pixel + c;
-      const auto pixel_value = mzlib::grid::access(drawing, drawing_pixel);
-      if (operation == drawing_operation::add)
-         mzlib::grid::access(grid, canvas_pixel) += pixel_value;
-      else if (operation == drawing_operation::subtract)
-         mzlib::grid::access(grid, canvas_pixel) -= pixel_value;
-   }
-   while (move_to_next_cell(drawing_pixel, {0, 0}, drawing_size));
-
-   return true;
-}
-
-template <typename T>
-bool
 check_for_drawing (
    const mzlib::grid::type<T>& grid,
    const mzlib::grid::type<T>& drawing,
